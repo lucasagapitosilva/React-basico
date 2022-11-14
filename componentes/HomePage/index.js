89500a4a6c612354c09be176f86a8cb1472ca83b
Lucas Agapito <lucasbezerra.rocinha@gmail.com>
2022-11-14 17:33:19 -0300

    arquivos projeto

---
+++ componentes/HomePage/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Card from '../Card/';
+
+import './style.css';
+
+export default function HomePage() {
+    return (
+
+        <div className="home-page">
+            <Card />
+            <Card />
+            <Card />
+        </div>
+
+    );
+}
